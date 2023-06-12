a9eeda07a631e90ed7931b8655308ee070539c82
hkshakib <humayunkibriashakib@gmail.com>
2023-06-12 19:54:01 +0600

    fix: photo problems

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styles from "../Styles/Home.module.css";
 
-function Home() {
+const Home = () => {
+  const [biddingProducts, setBiddingProducts] = useState([]);
+
+  useEffect(() => {
+    fetchBiddingProducts();
+  }, []);
+
+  const fetchBiddingProducts = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/auction/api/products/");
+      if (response.ok) {
+        const data = await response.json();
+        setBiddingProducts(data);
+      } else {
+        console.log("Error fetching bidding products");
+      }
+    } catch (error) {
+      console.log("Error fetching bidding products:", error);
+    }
+  };
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.container}>
+      <h2>Bidding Products</h2>
+      {biddingProducts.length === 0 ? (
+        <p>No products available.</p>
+      ) : (
+        <div className={styles.productList}>
+          {biddingProducts.map((product) => (
+            <div key={product.id} className={styles.productItem}>
+              
+              <img src={`http://127.0.0.1:8000${product.photo}`} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <div className={styles.bidInfo}>
+                <p>Current Bid: ${product.highest_bid}</p>
+                <p>Ends At: {product.auction_end_date_time}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Home;
