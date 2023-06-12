27dd0bc4126570a57154355abfcfdfeb638728a6
hkshakib <humayunkibriashakib@gmail.com>
2023-06-13 01:31:39 +0600

    feat: added bidder tables on every products page

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -7,10 +7,12 @@ const ProductDetails = () => {
   const { pk } = useParams();
   const [product, setProduct] = useState(null);
   const [bidPrice, setBidPrice] = useState(0);
+  const [bids, setBids] = useState([]);
   const { email, user } = useContext(AuthContext);
 
   useEffect(() => {
     fetchProductDetails();
+    fetchBids();
   }, []);
 
   const fetchProductDetails = async (e) => {
@@ -27,6 +29,21 @@ const ProductDetails = () => {
       console.log("Error fetching product details:", error);
     }
   };
+
+  const fetchBids = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/auction/api/bid/${pk}`);
+      if (response.ok) {
+        const data = await response.json();
+        setBids(data);
+      } else {
+        console.log("Error fetching bids");
+      }
+    } catch (error) {
+      console.log("Error fetching bids:", error);
+    }
+  };
+
 
   const handleBid = async (e) => {
     e.preventDefault();
@@ -46,7 +63,7 @@ const ProductDetails = () => {
       });
 
       if (response.ok) {
-        // Bid was successful, update the product details or show a success message
+        window.location.reload();
       } else {
         console.log('Error submitting bid');
       }
@@ -59,36 +76,70 @@ const ProductDetails = () => {
     setBidPrice(parseInt(event.target.value));
   };
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    return date.toLocaleTimeString(undefined, options);
+  };
+
   return (
     <div>
       {product ? (
         <div className={styles.container}>
-          <div>
-            <img src={`http://127.0.0.1:8000${product.photo}`} alt={product.title} className={styles.productImage} />
+            <div>
+              <img src={`http://127.0.0.1:8000${product.photo}`} alt={product.title} className={styles.productImage} />
+            </div>
+              <div>
+                  <h2 className={styles.productTitle}>{product.title}</h2>
+                  <p className={styles.productDescription}>{product.description}</p>
+                  <p className={styles.productPrice}>Price: ${product.highest_bid}</p>
+                  <p className={styles.productEndsAt}>Ends At: {product.auction_end_date_time}</p>
+                  <p className={styles.productBidder}>Bidder: {product.email}</p>
+                  <div className={styles.bidSection}>
+                  <input
+                    type="number"
+                    value={bidPrice}
+                    onChange={handleBidPriceChange}
+                    className={styles.bidInput}
+                  />
+                  <button className={styles.bidBtn} onClick={handleBid}>
+                    Bid
+                  </button>
+              </div>
+            </div> <br/>
+            {bids.length > 0 && (
+            <div className={styles.bidsTable}>
+              <h3 className={styles.tableTitle}>Bids:</h3>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Bidder</th>
+                    <th>Bidding Amount</th>
+                    <th>Bid End Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bids?.map((bid) => (
+                    <tr key={bid.id}>
+                      <td>{bid.email}</td>
+                      <td>{bid.amount}</td>
+                      <td>{formatDate(bid.created_at)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
           </div>
-          <div>
-              <h2 className={styles.productTitle}>{product.title}</h2>
-              <p className={styles.productDescription}>{product.description}</p>
-              <p className={styles.productPrice}>Price: ${product.highest_bid}</p>
-              <p className={styles.productEndsAt}>Ends At: {product.auction_end_date_time}</p>
-              <p className={styles.productBidder}>Bidder: {product.email}</p>
-              <div className={styles.bidSection}>
-              <input
-                type="number"
-                value={bidPrice}
-                onChange={handleBidPriceChange}
-                className={styles.bidInput}
-              />
-              <button className={styles.bidBtn} onClick={handleBid}>
-                Bid
-              </button>
-          </div>
-          
-          </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      ) : (<p>Loading...</p> )}
+
     </div>
   );
 };
